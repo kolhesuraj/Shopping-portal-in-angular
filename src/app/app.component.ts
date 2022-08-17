9fe64b387785bdf9f4fecac074ac8b7e5f962004
AM-PC-05 <surajkolhe214@gmail.com>
2022-08-17 10:29:26 +0530

    spinner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,Event, NavigationStart, NavigationEnd } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +7,18 @@ import { Router,Event, NavigationStart, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
-  show!:boolean
-  constructor( private router:Router){
-  
-    this.router.events.subscribe((roterEvent : Event)=>{
-      if(roterEvent instanceof NavigationStart){
-              this.show = true
+  show!: boolean;
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((roterEvent: Event) => {
+      if (roterEvent instanceof NavigationStart) {
+        this.show = true;
       }
-       if (roterEvent instanceof NavigationEnd) {
-         this.show = false;
-       }
-
+      if (roterEvent instanceof NavigationEnd) {
+        this.show = false;
+      }
     });
   }
-
 
   title = 'Project';
 }
