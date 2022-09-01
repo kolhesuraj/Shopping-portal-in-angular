33c4ddafbc1ef7ab2081390bc57322f5f1aff5ad
suraj kolhe <surajkolhe214@gmail.com>
2022-09-01 16:03:03 +0530

    email varification for user added

--- src/app/services/lodder.interceptor.spec.ts
+++ src/app/services/lodder.interceptor.spec.ts
@@ -3,11 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { LodderInterceptor } from './lodder.interceptor';
 
 describe('LodderInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      LodderInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [LodderInterceptor],
+    })
+  );
 
   it('should be created', () => {
     const interceptor: LodderInterceptor = TestBed.inject(LodderInterceptor);
