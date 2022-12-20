940ad518875b4ffb6b7977400f760b87e35d939d
Kolhesuraj <surajkolhe214@gmail.com>
2022-12-20 17:42:02 +0530

    spinner for navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpRequest } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { LoginService } from './services/login.service';
 
@@ -10,14 +12,38 @@ import { LoginService } from './services/login.service';
 })
 export class AppComponent implements OnInit {
   spinner: boolean = false;
-  constructor(public ls: LoginService) {}
+  spinner2: boolean = false;
+  constructor(
+    public ls: LoginService,
+    private router: Router,
+    private titleService: Title
+  ) {}
   ngOnInit(): void {
     this.ls.loader.subscribe({
-      next: (res)=>{
-        this.spinner = res
+      next: (res) => {
+        this.spinner = res;
+      },
+    });
+
+    this.router.events.subscribe((e: any) => {
+      if (e instanceof NavigationStart) {
+        this.spinner2 = true;
       }
-    })
+      if (e instanceof NavigationEnd) {
+        this.spinner2 = false;
+        // localStorage.setItem('url', e.urlAfterRedirects);
+        let name = e.urlAfterRedirects;
+        if (name) {
+          let url = name.split('/');
+
+          if (url.length < 4) {
+            this.titleService.setTitle(url[1] + ' | ' + url[2]);
+          } else {
+            this.titleService.setTitle(url[1] + ' | ' + url[3]);
+          }
+        }
+      }
+    });
   }
   title = 'Project';
-  
 }
